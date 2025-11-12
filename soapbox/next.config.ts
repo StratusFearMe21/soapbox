3300fa5ad2715168e06dce6d4c8c6e6f07d22e56
Isaac Mills <rooster0055@protonmail.com>
2025-11-12 16:55:35 -0700

    Fix pipeline

--- soapbox/next.config.ts
+++ soapbox/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: process.env.PAGES_OUTPUT,
-  basePath: process.env.PAGES_BASE_PATH,
+  distDir: process.env.PAGES_OUTPUT || "dist",
+  basePath: process.env.PAGES_BASE_PATH || "",
 };
 
 export default nextConfig;
