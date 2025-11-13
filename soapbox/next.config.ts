477404fcb3348b8090a7d5d54b986c7143910e9e
Isaac Mills <rooster0055@protonmail.com>
2025-11-12 17:18:35 -0700

    Add lint and finish deploy steps in pipeline

--- soapbox/next.config.ts
+++ soapbox/next.config.ts
@@ -2,8 +2,8 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'export',
-  distDir: process.env.PAGES_OUTPUT || "dist",
-  basePath: process.env.PAGES_BASE_PATH || "",
+  distDir: "build/dist",
+  basePath: process.env.PAGES_BASE_PATH,
 };
 
 export default nextConfig;
