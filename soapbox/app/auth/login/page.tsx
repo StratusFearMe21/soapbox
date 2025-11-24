46fa9fab36b5425a26e9b9cec7f819710700d1dd
Melanie Murray <mmurray@Mac.localdomain>
2025-11-23 19:25:22 -0700

    chore: remove logos, edit css

--- soapbox/app/auth/login/page.tsx
+++ soapbox/app/auth/login/page.tsx
@@ -1,11 +1,25 @@
-import { LoginForm } from "@/components/login-form";
+import {LoginForm} from "@/components/login-form";
+import Link from "next/link";
+import {DeployButton} from "@/components/deploy-button";
+import {hasEnvVars} from "@/lib/utils";
+import {EnvVarWarning} from "@/components/env-var-warning";
+import {AuthButton} from "@/components/auth-button";
 
 export default function Page() {
-  return (
-    <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm">
-        <LoginForm />
-      </div>
-    </div>
-  );
+    return (
+        <div>
+            <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
+                <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
+                    <div className="flex gap-5 items-center font-semibold">
+                        <Link href="/">Home Page</Link>
+                    </div>
+                </div>
+            </nav>
+            <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+                <div className="w-full max-w-sm">
+                    <LoginForm/>
+                </div>
+            </div>
+        </div>
+    );
 }
