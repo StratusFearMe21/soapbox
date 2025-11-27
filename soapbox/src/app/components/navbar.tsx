5f339ff56f25f3549e9580f171b8c4497a7a6d7f
Jake K <111465068+xatikat@users.noreply.github.com>
2025-11-26 19:52:30 -0700

    navbar fix-up and concept / non-final styling

--- soapbox/src/app/components/navbar.tsx
+++ soapbox/src/app/components/navbar.tsx
@@ -5,13 +5,16 @@ import {useEffect, useState} from "react";
 import {createClient} from "@/app/utils/supabase/client";
 import {Button} from "@/app/components/ui/button";
 import {ThoughtPostBoxOverlay} from "@/app/components/thought-postbox-overlay";
+import {SquarePen} from "lucide-react";
+import {Badge} from "@/app/components/ui/badge";
+import {Card} from "@/app/components/ui/card";
+import {Label} from "@/app/components/ui/label";
 
-const linkClass = "pt-4 pb-4 text-center w-full font-bold transition-colors duration-350 hover:bg-slate-900"
-const buttonClass = linkClass + " ";
+const badgeClass = "size-4 pt-4 pb-4 w-full font-bold hover:bg-black/20 m-2 text-sm"
 const transitionGroup = "transition-transform duration-500 translate-x-[-150%] group-hover:translate-x-0"
 
 export default function Navbar() {
-  const [ profileName, setProfileName ] = useState('Profile');
+  const [ profileName, setProfileName ] = useState('My Profile');
   const [ profileLink, setProfileLink ] = useState('/login');
 
   useEffect(() => {
@@ -35,16 +38,12 @@ export default function Navbar() {
   const [ isShown, setIsShown ] = useState(false);
 
   const onOpenThoughtPostBox = async () => {
-    console.log("test on show post box")
-
     if (!isShown) {
       setIsShown(true);
     }
   }
 
   const onCloseThoughtPostBox = async () => {
-    console.log("test on close post box")
-
     if (isShown) {
       setIsShown(false);
     }
@@ -59,14 +58,35 @@ export default function Navbar() {
         onOpen={onOpenThoughtPostBox}
         onClose={onCloseThoughtPostBox}
       />
-      <div className={
-        "flex flex-col justify-center items-start border-4 w-full bg-background"
+      <Card className={
+        "flex flex-col border-4 w-full justify-center items-center p-2"
       }>
-        <Link className={linkClass} href="/">Feed (blank)</Link>
-        <Link className={linkClass} href="/test">Posting Test</Link>
-        <Link className={linkClass} href={profileLink}>{profileName}</Link>
-        <Button className={linkClass} onClick={onOpenThoughtPostBox}>+</Button>
-      </div>
+
+        <Badge className={badgeClass + " rounded-lg"} asChild>
+          <Link href={"/feed"}>
+            Feed
+          </Link>
+        </Badge>
+
+        <Badge className={badgeClass + " rounded-lg"} asChild>
+          <Link href={"/test"}>
+            Test Page
+          </Link>
+        </Badge>
+
+        <Badge className={badgeClass + " rounded-lg"} asChild>
+          <Link href={profileLink}>
+            {profileName}
+          </Link>
+        </Badge>
+
+        <div className={"border-4 w-full m-1"}></div>
+
+        <Button className={badgeClass + " size-4 ease-in-out hover:w-full transition-all duration-200 group"} onClick={onOpenThoughtPostBox}>
+          <SquarePen className={"group-hover:opacity-0 transition-discrete duration-300"} strokeWidth={2.8}/>
+          <Label className={"font-bold absolute opacity-0 group-hover:opacity-100 transition-opacity duration-75 group-hover:duration-700"}>Create Thought</Label>
+        </Button>
+      </Card>
     </div>
   )
 }
