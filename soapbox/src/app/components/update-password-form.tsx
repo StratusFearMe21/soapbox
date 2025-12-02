aae6aac7ed2d4a9a3efe98c1b20eb7b1310ee91a
Jake K <111465068+xatikat@users.noreply.github.com>
2025-12-01 18:33:12 -0700

    fix update-password-form.tsx

--- soapbox/src/app/components/update-password-form.tsx
+++ soapbox/src/app/components/update-password-form.tsx
@@ -1,17 +1,17 @@
 "use client";
 
-import { cn } from "@/lib/utils";
-import { createClient } from "@/lib/supabase/client";
-import { Button } from "@/components/ui/button";
+import { cn } from "@/app/utils/utils";
+import { createClient } from "@/app/utils/supabase/client";
+import { Button } from "@/app/components/ui/button";
 import {
   Card,
   CardContent,
   CardDescription,
   CardHeader,
   CardTitle,
-} from "@/components/ui/card";
-import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+} from "@/app/components/ui/card";
+import { Input } from "@/app/components/ui/input";
+import { Label } from "@/app/components/ui/label";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
 
