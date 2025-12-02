769d6f0135150c1f623bfa59db05b47ed575d541
Jake K <111465068+xatikat@users.noreply.github.com>
2025-12-01 23:33:38 -0700

    re-adding /auth/ and fixing paths once more

--- soapbox/src/app/components/login-form.tsx
+++ soapbox/src/app/components/login-form.tsx
@@ -74,7 +74,7 @@ export function LoginForm({
                 <div className="flex items-center">
                   <Label htmlFor="password">Password</Label>
                   <Link
-                    href="/forgot-password"
+                    href="/auth/forgot-password"
                     className="ml-auto inline-block text-sm underline-offset-4 hover:underline"
                   >
                     Forgot your password?
@@ -97,7 +97,7 @@ export function LoginForm({
             <div className="mt-4 text-center text-sm">
               Don&apos;t have an account?{" "}
               <Link
-                href="/sign-up"
+                href="/auth/sign-up"
                 className="underline underline-offset-4"
               >
                 Sign up here
