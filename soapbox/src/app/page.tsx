fab0d31b1c3d498665804b762b26d8889612a5e2
Melanie Murray <mmurray@Melanies-MacBook-Air.local>
2025-12-08 18:36:01 -0700

    fix: clean up home page

--- soapbox/src/app/page.tsx
+++ soapbox/src/app/page.tsx
@@ -1,4 +1,3 @@
-import { DeployButton } from "@/app/components/deploy-button";
 import { AuthButton } from "@/app/components/auth-button";
 import { ThemeSwitcher } from "@/app/components/theme-switcher";
 import Link from "next/link";
@@ -12,34 +11,14 @@ export default function Home() {
         <nav className="w-full flex justify-center border-b border-b-foreground/10 h-16">
           <div className="w-full max-w-5xl flex justify-between items-center p-3 px-5 text-sm">
             <div className="flex gap-5 items-center font-semibold">
-              {/*<Link href={"/"}>Next.js Supabase Starter</Link>*/}
               <Link href="/">Home Page</Link>
-              <div className="flex items-center gap-2">
-                <DeployButton />
-              </div>
+
             </div>
             <AuthButton />
           </div>
         </nav>
-        <div className="flex-1 flex flex-col gap-20 max-w-5xl p-5">
-          {/*<Hero />*/}
-          <main className="flex-1 flex flex-col gap-6 px-4">
-            <h2 className="font-medium text-xl mb-4">Next steps</h2>
-          </main>
-        </div>
 
         <footer className="w-full flex items-center justify-center border-t mx-auto text-center text-xs gap-8 py-16">
-          <p>
-            Powered by{" "}
-            <a
-              href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-              target="_blank"
-              className="font-bold hover:underline"
-              rel="noreferrer"
-            >
-              Supabase
-            </a>
-          </p>
           <ThemeSwitcher />
         </footer>
       </div>
