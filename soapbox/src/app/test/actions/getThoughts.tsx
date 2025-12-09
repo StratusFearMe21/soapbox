28a57fbeaff98ec3fac7d7893133ae929aed0e20
Isaac Mills <rooster0055@protonmail.com>
2025-12-08 19:39:28 -0700

    Push latest stuff

--- soapbox/src/app/test/actions/getThoughts.tsx
+++ soapbox/src/app/test/actions/getThoughts.tsx
@@ -3,6 +3,7 @@ import { createClient } from "@/app/utils/supabase/server";
 
 export async function GetThoughts() {
   const supabase = await createClient();
-  const { data, error } = await supabase.from('thoughts_test').select('*');
+  const { data, error } = await supabase.from('thoughts_test').select('*,profiles!thoughts_test_user_id_fkey1(nickname,username)');
+  console.log(error)
   return data ? data : [];
 }
