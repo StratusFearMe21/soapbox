8d3b83d41cbe4e95c8347e84c9006e5149d952e5
Jake K <111465068+xatikat@users.noreply.github.com>
2025-12-10 18:28:21 -0700

    fix delete button dialog

--- soapbox/src/app/components/ui/confirm-dialog.tsx
+++ soapbox/src/app/components/ui/confirm-dialog.tsx
@@ -7,7 +7,6 @@ import {
   DialogTrigger
 } from "@/app/components/ui/dialog";
 import {Button} from "@/app/components/ui/button";
-import {ReactNode} from "react";
 
 interface ConfirmDialogProps {
   buttonText?: string;
@@ -15,30 +14,19 @@ interface ConfirmDialogProps {
   confirmText: string,
   cancelText: string,
   onConfirm: () => void,
-  children: ReactNode,
 }
 
 export function ConfirmDialog(
-  { buttonText, mainText, confirmText, cancelText, onConfirm, children }: ConfirmDialogProps
+  { buttonText, mainText, confirmText, cancelText, onConfirm } : ConfirmDialogProps
 )
 {
   return (
     <Dialog>
       <DialogTrigger asChild>
-        {
-          buttonText ?
             <Button
               variant={"glass"}
               className={"text-xs w-fit h-6 m-0"}
             >{buttonText}</Button>
-          :
-            <div
-              className="ring-offset-background focus:ring-ring data-[state=open]:text-muted-foreground absolute top-4 right-4 rounded-xs opacity-100 transition-transform hover:scale-120 focus:ring-2 focus:ring-offset-2 focus:outline-hidden disabled:pointer-events-none [&_svg]:pointer-events-none [&_svg]:shrink-0 [&_svg:not([class*='size-'])]:size-4"
-            >
-              {children}
-            </div>
-        }
-
       </DialogTrigger>
       <DialogContent className={"glass"}>
         <DialogTitle hidden/>
